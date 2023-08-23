1115238e100882c5811401e368f617e7ee5a1e60
hasanoqool <hasanoqool98@gmail.com>
2023-08-23 16:51:07 +0300

    basic image classifier

---
+++ image_classifier.py
@@ -0,0 +1,15 @@
+"""
+CONTACT:
+Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
+"""
+
+#===============Import the required libraries===============
+import glob
+import os
+import tarfile
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.utils import get_file
+#===============Import the required libraries===============
+
+
