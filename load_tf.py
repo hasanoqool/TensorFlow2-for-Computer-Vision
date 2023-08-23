793859d0a3244c53ea8b3aa46f1b9e5dae1fa2f0
hasanoqool <hasanoqool98@gmail.com>
2023-08-23 13:42:53 +0300

    load images using TensorFlow API

--- load_tf.py
+++ load_tf.py
@@ -3,3 +3,13 @@ CONTACT:
 Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
 """
 
+#===============Import the required libraries===============
+import os
+import tarfile
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from keras.utils import get_file
+#===============Import the required libraries===============
+
+
