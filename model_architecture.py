45813fe8cdb29679dd309a52a409687eb3774c6f
hasanoqool <hasanoqool98@gmail.com>
2023-08-23 15:55:32 +0300

    visualizing a model's architecture

---
+++ model_architecture.py
@@ -0,0 +1,11 @@
+"""
+CONTACT:
+Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
+"""
+
+#===============Import the required libraries===============
+from PIL import Image
+from keras import Model
+from keras.layers import * 
+from keras.utils import plot_model
+#===============Import the required libraries===============
